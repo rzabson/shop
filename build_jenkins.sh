ffdba38885525ecc828c93448d19e3ee22ae5bf1
Julia Komorowska <komolcia@gmail.com>
2024-10-28 15:38:27 +0100

    Jenkins and github configuration - added Jenkinsfile

--- build_jenkins.sh
+++ build_jenkins.sh
@@ -1,1 +1,2 @@
 docker buildx build --platform linux/amd64 -t my-jenkins:buildx-latest . &&  docker compose  up -d  my-jenkins
+docker network create jenkins-sonarqube
--- install_k3s.sh
+++ install_k3s.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 curl -sfL https://get.k3s.io | sh -
 sudo kubectl get nodes
+sudo systemctl status k3s
+sudo systemctl restart k3s
